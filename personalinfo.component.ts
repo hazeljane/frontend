ae7225d1f49c6d590ecb55772c559fa4c012e320
hazeljane <gunohazeljane@gmail.com>
2025-05-06 17:26:51 +0700

    Add files via upload

---
+++ personalinfo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personalinfo',
+  imports: [],
+  templateUrl: './personalinfo.component.html',
+  styleUrl: './personalinfo.component.scss'
+})
+export class PersonalinfoComponent {
+
+}
